d46ac4534217a4607eb162c63c55014e2d0f54ef
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2024-04-04 09:45:56 +0200

    Refactored style

--- src/main/java/com/jabberpoint/Style.java
+++ src/main/java/com/jabberpoint/Style.java
@@ -17,44 +17,54 @@ import java.awt.Font;
  */
 
 public class Style {
-	private static Style[] styles; // de styles
-	
-	private static final String FONTNAME = "Helvetica";
-	int indent;
-	Color color;
-	Font font;
-	int fontSize;
-	int leading;
+    private static Style[] styles;
 
-	public static void createStyles() {
-		styles = new Style[5];    
-		// The styles are fixed.
-		styles[0] = new Style(0, Color.red,   48, 20);	// style for item-level 0
-		styles[1] = new Style(20, Color.blue,  40, 10);	// style for item-level 1
-		styles[2] = new Style(50, Color.black, 36, 10);	// style for item-level 2
-		styles[3] = new Style(70, Color.black, 30, 10);	// style for item-level 3
-		styles[4] = new Style(90, Color.black, 24, 10);	// style for item-level 4
-	}
+    private static final String FONT_NAME = "Helvetica";
+    private final int indent, fontSize, leading;
+    private final Color color;
+    private final Font font;
 
-	public static Style getStyle(int level) {
-		if (level >= styles.length) {
-			level = styles.length - 1;
-		}
-		return styles[level];
-	}
+    public static void createStyles() {
+        styles = new Style[5];
+        // The styles are fixed.
+        styles[0] = new Style(0, Color.red, 48, 20);    // style for item-level 0
+        styles[1] = new Style(20, Color.blue, 40, 10);  // style for item-level 1
+        styles[2] = new Style(50, Color.black, 36, 10); // style for item-level 2
+        styles[3] = new Style(70, Color.black, 30, 10); // style for item-level 3
+        styles[4] = new Style(90, Color.black, 24, 10); // style for item-level 4
+    }
 
-	public Style(int indent, Color color, int points, int leading) {
-		this.indent = indent;
-		this.color = color;
-		font = new Font(FONTNAME, Font.BOLD, fontSize=points);
-		this.leading = leading;
-	}
+    public static Style getStyle(int level) {
+        if (level >= styles.length) {
+            level = styles.length - 1;
+        }
+        return styles[level];
+    }
 
-	public String toString() {
-		return "["+ indent + "," + color + "; " + fontSize + " on " + leading +"]";
-	}
+    public Style(int indent, Color color, int points, int leading) {
+        this.indent = indent;
+        this.color = color;
+        font = new Font(FONT_NAME, Font.BOLD, fontSize = points);
+        this.leading = leading;
+    }
 
-	public Font getFont(float scale) {
-		return font.deriveFont(fontSize * scale);
-	}
+    public String toString() {
+        return String.format("[%d,%s; %d on %d]", indent, color, fontSize, leading);
+    }
+
+    public Font getFont(float scale) {
+        return font.deriveFont(fontSize * scale);
+    }
+    
+    public int getIndent() {
+        return indent;
+    }
+    
+    public Color getColor() {
+        return color;
+    }
+
+    public int getLeading() {
+        return leading;
+    }
 }

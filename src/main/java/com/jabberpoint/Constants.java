6cb5ebcb87d9210b241655a77ccfd74697c8a99d
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2024-04-04 23:55:28 +0200

    Simplified constants

--- src/main/java/com/jabberpoint/Constants.java
+++ src/main/java/com/jabberpoint/Constants.java
@@ -4,38 +4,32 @@ public class Constants {
     // Jabberpoint version string
     public static final String JAB_VERSION = "Jabberpoint 1.6 - OU version";
     // Error messages
-    public static final String UNKNOWN_TYPE = "Unknown Element type";
-    public static final String NFE = "Number Format Exception";
-    public static final String IO_EX = "IO Exception: ";
-    public static final String LOAD_ERR = "Load Error";
-    public static final String SAVE_ERR = "Save Error";
-    public static final String IO_ERR = "IO Error: ";
-    public static final String JABBER = "Jabberpoint Error ";
-    public static final String NOT_FOUND = " not found";
-    public static final String FILE_ERROR = "File ";
+    public static class Error {
+        public static final String NFE = "Number Format Exception";
+        public static final String IO_EX = "IO Exception: ";
+        public static final String LOAD_ERR = "Load Error";
+        public static final String SAVE_ERR = "Save Error";
+        public static final String IO_ERR = "IO Error: ";
+        public static final String JABBER = "Jabberpoint Error ";
+        public static final String NOT_FOUND = " not found";
+        public static final String FILE_ERROR = "File ";
+    }
     // Menu Controller options
-    public static final String ABOUT = "About";
-    public static final String FILE = "File";
-    public static final String EXIT = "Exit";
-    public static final String GOTO = "Go to";
-    public static final String HELP = "Help";
-    public static final String NEW = "New";
-    public static final String NEXT = "Next";
-    public static final String OPEN = "Open";
-    public static final String PAGE_NR = "Page number?";
-    public static final String PREV = "Prev";
-    public static final String SAVE = "Save";
-    public static final String VIEW = "View";
+    public static class Menu {
+        public static final String ABOUT = "About";
+        public static final String FILE = "File";
+        public static final String EXIT = "Exit";
+        public static final String GOTO = "Go to";
+        public static final String HELP = "Help";
+        public static final String NEW = "New";
+        public static final String NEXT = "Next";
+        public static final String OPEN = "Open";
+        public static final String PAGE_NR = "Page number?";
+        public static final String PREV = "Prev";
+        public static final String SAVE = "Save";
+        public static final String VIEW = "View";
+    }
     // File names
     public static final String TEST_FILE = "test.xml";
     public static final String SAVE_FILE = "dump.xml";
-    // XML Accessor Constants
-    public static final String SHOW_TITLE = "showtitle";
-    public static final String SLIDE_TITLE = "title";
-    public static final String SLIDE = "slide";
-    public static final String ITEM = "item";
-    public static final String LEVEL = "level";
-    public static final String KIND = "kind";
-    public static final String TEXT = "text";
-    public static final String IMAGE = "image";
 }

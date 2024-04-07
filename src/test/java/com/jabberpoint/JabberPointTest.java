d01b679b0bf6e83a80f31cc73a59b85d461b473d
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2024-04-07 21:09:51 +0200

    Added check for headless environment

--- src/test/java/com/jabberpoint/JabberPointTest.java
+++ src/test/java/com/jabberpoint/JabberPointTest.java
@@ -2,21 +2,36 @@ package com.jabberpoint;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 import static com.jabberpoint.TestUtils.getTestXmlPath;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class JabberPointTest {
     @Test
     public void testMain() {
-        JabberPoint.main(new String[] { getTestXmlPath() });
+        if (!GraphicsEnvironment.isHeadless()) {
+            JabberPoint.main(new String[] { getTestXmlPath() });
+        } else {
+            assertThat(GraphicsEnvironment.isHeadless()).isTrue();
+        }
     }
 
     @Test
     public void testMainNoArgs() {
-        JabberPoint.main(new String[] {});
+        if (!GraphicsEnvironment.isHeadless()) {
+            JabberPoint.main(new String[] {});
+        } else {
+            assertThat(GraphicsEnvironment.isHeadless()).isTrue();
+        }
     }
 
     @Test
     public void testMainInvalidArgs() {
-        JabberPoint.main(new String[] { "invalid" });
+        if (!GraphicsEnvironment.isHeadless()) {
+            JabberPoint.main(new String[] { "invalid" });
+        } else {
+            assertThat(GraphicsEnvironment.isHeadless()).isTrue();
+        }
     }
 }
